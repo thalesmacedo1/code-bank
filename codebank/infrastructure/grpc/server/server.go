3b4c4a6da0a94671c9173fb266e113148e564e74
Thales Macêdo <thalesmacedo1@live.com>
2022-08-17 13:07:27 -0300

    created grpc server

---
+++ codebank/infrastructure/grpc/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"log"
+	"net"
+
+	"github.com/thalesmacedo1/code-bank/infrastructure/grpc/pb"
+	"github.com/thalesmacedo1/code-bank/infrastructure/grpc/service"
+	"github.com/thalesmacedo1/code-bank/usecase"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type GRPCServer struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+}
+
+func NewGRPCServer() GRPCServer {
+	return GRPCServer{}
+}
+
+func (g GRPCServer) Serve() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("could not listen tpc port")
+	}
+	transactionService := service.NewTransactionService()
+	transactionService.ProcessTransactionUseCase = g.ProcessTransactionUseCase
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterPaymentServiceServer(grpcServer, transactionService)
+	grpcServer.Serve(lis)
+}
